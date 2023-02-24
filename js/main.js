96bee30ae8e15528cf9c0862573d3a39b5e0bd7e
Marco Rinaldi <marcorinaldi98@libero.it>
2023-02-24 14:47:36 +0100

    Struttura e logica completati!

--- js/main.js
+++ js/main.js
@@ -11,6 +11,22 @@ let max = 100;
 
 //ciclo per generare i numeri necessari
 for (let i = min; i <= max; i++) {
-
-    console.log(i);
+    
+    // se i è moltiplicabile per 15 scrivi Fizz Buzz
+    if (i % 15 == 0) {
+        const square = `<div class="ms_square ms_bg_4">Fizz Buzz</div>`;
+        container.innerHTML += square;
+    // se i è moltiplicabile per 5 scrivi Buzz
+    } else if (i % 5 == 0) {
+        const square = `<div class="ms_square ms_bg_3">Buzz</div>`;
+        container.innerHTML += square;
+    // se i è moltiplicabile per 3 scrivi Fizz
+    } else if (i % 3 == 0) {
+        const square = `<div class="ms_square ms_bg_2">Fizz</div>`;
+        container.innerHTML += square;
+    // ALTRIMENTI scrivi il numero che corrisponde a i
+    } else {
+        const square = `<div class="ms_square ms_bg_1">${i}</div>`;
+        container.innerHTML += square;
+    }  
 }
