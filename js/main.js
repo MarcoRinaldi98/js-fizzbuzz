712855cb2f5ce4cfeeb0c3923eb28b3afc060a9c
Marco Rinaldi <marcorinaldi98@libero.it>
2023-02-24 13:15:53 +0100

    Genero i numeri da 1 a 100

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ JAVASCRIPT
 */
 
 //definisco le variabili
+const container = document.querySelector('.container-sm')
+
+let min = 1;
+
+let max = 100;
+
+//ciclo per generare i numeri necessari
+for (let i = min; i <= max; i++) {
+
+    console.log(i);
+}
