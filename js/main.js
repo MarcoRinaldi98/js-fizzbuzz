5af457fbfe69e1aace6bc8260616d529a514a321
Marco Rinaldi <marcorinaldi98@libero.it>
2023-02-27 10:17:55 +0100

    Abbreviazione del codice

--- js/main.js
+++ js/main.js
@@ -11,35 +11,28 @@ let max = 100;
 
 //ciclo per generare i numeri necessari
 for (let i = min; i <= max; i++) {
+
+    const square = document.createElement('div');
+    square.classList.add('ms_square');
     
     // se i è moltiplicabile per 15 scrivi Fizz Buzz
     if (i % 15 == 0) {
-        const square = document.createElement('div');
-        square.classList.add('ms_square');
         square.classList.add('ms_bg_4');
         square.append(`Fizz Buzz`);
-        container.append(square);
     // se i è moltiplicabile per 5 scrivi Buzz
     } else if (i % 5 == 0) {
-        const square = document.createElement('div');
-        square.classList.add('ms_square');
         square.classList.add('ms_bg_3');
         square.append(`Buzz`);
-        container.append(square);
     // se i è moltiplicabile per 3 scrivi Fizz
     } else if (i % 3 == 0) {
-        const square = document.createElement('div');
-        square.classList.add('ms_square');
         square.classList.add('ms_bg_2');
         square.append(`Fizz`);
-        container.append(square);
     // ALTRIMENTI scrivi il numero che corrisponde a i
     } else {
-        const square = document.createElement('div');
-        square.classList.add('ms_square');
         square.classList.add('ms_bg_1');
         square.append(i);
-        container.append(square);
     }  
+
+    container.append(square);
 }
 
